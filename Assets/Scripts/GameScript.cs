eb4d8984af8ab15deb49d807c97d459daf985847
Phantom <nikitka.dankin@mail.ru>
2019-03-13 17:56:44 +0300

    test

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    [SerializeField] GameObject targetPref;
+    [SerializeField] Text textPoints;
+    [SerializeField] GameObject endPanel;
+    List<GameObject> targets = new List<GameObject>();
+    int points = 0;
+    const float offsetX = -4.5f;
+    const float offsetY = -4;
+    const float stepX = 2.2f;
+    const float stepY = 2;
+    List<Dictionary<int, bool>> positions = new List<Dictionary<int, bool>>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            positions.Add(new Dictionary<int, bool>());
+            for (int j = 0; j < 5; j++) positions[i].Add(j, false);
+        }
+        Restart();
+    }
+
+    public void Restart()
+    {
+        AddPoints(0);
+        endPanel.SetActive(false);
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++) positions[i][j] = false;
+        }
+        StartCoroutine(Spawn());
+    }
+
+    bool CheckEnd()
+    {
+        int n = 0;
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
+                if (positions[i][j]) n++;
+        if (n == 25) return true;
+        return false;
+    }
+
+    bool CheckRow(int i)
+    {
+        int n = 0;
+        for (int j = 0; j < 5; j++)
+            if (positions[i][j]) n++;
+        if (n == 5) return true;
+        return false;
+    }
+
+    IEnumerator Spawn()
+    {
+        while (!CheckEnd())
+        {
+            int m = Random.Range(0, 5);
+            while(CheckRow(m)) m = Random.Range(0, 5);
+            int n = Random.Range(0, 5);
+            while(positions[m][n]) n = Random.Range(0, 5);
+            targets.Add(Instantiate(targetPref, new Vector2(offsetX + n * stepX, offsetY + m * stepY), Quaternion.identity));
+            targets[targets.Count - 1].GetComponent<TargetScript>().gameManager = gameObject;
+            positions[m][n] = true;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    public void AddPoints(int n)
+    {
+        points += n;
+        textPoints.text = "Очки: " + points;
+    }
+
+    public void GameOver()
+    {
+        endPanel.SetActive(true);
+        StopAllCoroutines();
+        foreach (GameObject target in targets) Destroy(target);
+        targets.Clear();
+        points = 0;
+    }
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    [SerializeField] float speed;
+    public GameObject gameManager;
+    int price;
+    const float damage = -1f;
+    void Start()
+    {
+        int chance = Random.Range(0, 100);
+        if (chance < 40) 
+        {
+            price = 1;
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else if (chance < 70) 
+        {
+            price = 2;
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        else if (chance < 90) 
+        {
+            price = 3;
+            GetComponent<SpriteRenderer>().color = Color.magenta;
+        }
+        else if (chance < 100) 
+        {
+            price = 4;
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+    void Update()
+    {
+        if (transform.localScale.x < 10) transform.localScale += new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+        else gameManager.GetComponent<GameScript>().GameOver();
+    }
+
+    void OnMouseDown() 
+    {
+        transform.localScale += new Vector3(damage, damage, 0);
+        if (transform.localScale.x < 4) Destroy(gameObject);
+        gameManager.GetComponent<GameScript>().AddPoints(price);
+    }
+}
